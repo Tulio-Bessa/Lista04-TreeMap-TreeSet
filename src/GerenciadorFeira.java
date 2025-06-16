7e1e01cdf0c4b215eed1d1ffb6cf7d7bfd0faffe
Túlio Menezes Bessa <dev.tuliobessa@gmail.com>
2025-06-16 20:09:22 -0300

    .

--- src/GerenciadorFeira.java
+++ src/GerenciadorFeira.java
@@ -1,3 +1,55 @@
-public class GerenciadorFeira {
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
+public class GerenciadorFeira {
+    private TreeSet<Livro> todosOsLivros;
+    private TreeMap<String, TreeSet<String>> autoresELivros;
+
+    public GerenciadorFeira() {
+        this.todosOsLivros = new TreeSet<>();
+        this.autoresELivros = new TreeMap<>();
+    }
+
+
+    public void cadastrarLivro(String titulo, String autor, int ano) {
+        Livro novoLivro = new Livro(titulo, autor, ano);
+        todosOsLivros.add(novoLivro);
+        cadastrarAutor(autor, titulo);
+    }
+
+  
+     
+    private void cadastrarAutor(String autor, String titulo) {
+        
+        autoresELivros.computeIfAbsent(autor, k -> new TreeSet<>()).add(titulo);
+    }
+
+    
+    public void exibirTodosOsLivros() {
+        System.out.println("Todos os livros:");
+        if (todosOsLivros.isEmpty()) {
+            System.out.println("Nenhum livro cadastrado.");
+            return;
+        }
+        for (Livro livro : todosOsLivros) {
+            System.out.println(livro);
+        }
+        System.out.println(); 
+    }
+
+    public void exibirAutoresELivros() {
+        System.out.println("Autores e seus livros:");
+        if (autoresELivros.isEmpty()) {
+            System.out.println("Nenhum autor cadastrado.");
+            return;
+        }
+        for (Map.Entry<String, TreeSet<String>> entry : autoresELivros.entrySet()) {
+            System.out.println(entry.getKey() + ":");
+            for (String titulo : entry.getValue()) {
+                System.out.println("- " + titulo);
+            }
+            System.out.println();
+        }
+    }
 }
