d74e8b269fe06a858e11479c48cb638ffa9512f2
Túlio Menezes Bessa <dev.tuliobessa@gmail.com>
2025-06-18 19:02:47 -0300

    .

--- src/Livro.java
+++ src/Livro.java
@@ -1,5 +1,48 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Objects;
+
+public class Livro implements Comparable<Livro> {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + " (" + anoPublicacao + ")";
+    }
+
+    @Override
+    public int compareTo(Livro outro) {
+        return this.titulo.compareTo(outro.getTitulo());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(titulo, livro.titulo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        GerenciadorFeira feira = new GerenciadorFeira();
 
+        
+        feira.cadastrarLivro("Java Básico", "Ana Silva", 2015);
+        feira.cadastrarLivro("Algoritmos", "Carlos Souza", 2018);
+        feira.cadastrarLivro("Programação Java", "Ana Silva", 2019);
+
+        feira.exibirTodosOsLivros();
+        feira.exibirAutoresELivros();
+    }
 }
